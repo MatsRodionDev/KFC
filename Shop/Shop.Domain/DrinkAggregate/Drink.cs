48c5713c99b608f30a4974a1817a561077c7a658
MatsRodionDev <162904839+MatsRodionDev@users.noreply.github.com>
2025-09-28 12:42:01 +0300

    Create catalog project

---
+++ Shop/Shop.Domain/DrinkAggregate/Drink.cs
@@ -0,0 +1,41 @@
+﻿using Shop.Domain.Abstractions;
+using Shop.Domain.Enums;
+using Shop.Domain.ProductAggregate;
+
+namespace Shop.Domain.DrinkAggregate
+{
+    public class Drink : Aggregate
+    {
+        private Drink(
+            string name,
+            string description,
+            decimal price,
+            DrinkType type)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            Type = type;
+        }
+
+        public Drink() { }
+
+        public string Name { get; private set; } = string.Empty;
+        public string Description { get; private set; } = string.Empty;
+        public decimal Price { get; private set; }
+        public DrinkType Type { get; private set; }
+
+        public static Drink Create(
+            string name,
+            string description,
+            decimal price,
+            DrinkType type)
+        {
+            var drink = new Drink(name, description, price, type);
+
+            return drink;
+        }
+    }
+
+    
+}
---
+++ Shop/Shop.Domain/ToppingAggregate/Topping.cs
@@ -0,0 +1,33 @@
+﻿using Shop.Domain.Abstractions;
+
+namespace Shop.Domain.ToppingAggregate
+{
+    public class Topping : Entity
+    {
+        private Topping(
+            string name,
+            decimal price,
+            HashSet<Topping> availableForTypes)
+        {
+            Name = name;
+            Price = price;
+            AvailableForTypes = availableForTypes;
+        }
+
+        public Topping() { }
+
+        public string Name { get; private set; } = string.Empty;
+        public decimal Price { get; private set; }
+        public HashSet<Topping> AvailableForTypes { get; private set; } = [];
+
+        public static Topping Create(
+            string name,
+            decimal price,
+            HashSet<Topping> availableForTypes)
+        {
+            var topping = new Topping(name, price, availableForTypes);
+
+            return topping;
+        }
+    }
+}
