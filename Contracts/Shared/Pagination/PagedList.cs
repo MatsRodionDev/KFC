46adf354c76b8efd463df04f7897a75e552e9b05
mikhail-kochyn-devreon <mikhail.kochyn@devreon.com>
2025-10-26 19:49:58 +0300

    update contracts

---
+++ Contracts/Shared/Pagination/PagedList.cs
@@ -0,0 +1,3 @@
+﻿namespace Contracts.Shared.Pagination;
+
+public record PagedList<T>(List<T> Items, int PageNumber, int PageSize, int TotalCount);
---
+++ Contracts/Shared/Pagination/PaginationParameters.cs
@@ -0,0 +1,3 @@
+﻿namespace Contracts.Shared.Pagination;
+
+public record PaginationParameters(int PageNumber, int PageSize);
